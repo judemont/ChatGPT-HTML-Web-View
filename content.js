5d9e778d35096ace490125ebf6847cb6ecde0376
JdM <julien@rmbi.ch>
2024-03-21 13:04:55 +0100

    removed comments

--- content.js
+++ content.js
@@ -1,25 +1,3 @@
-// const PROMPT = `Act like a web developer, I will give you a website description and you will give me the html code. If you want to add css or javascript, put them in a <style> or <script> tag. Give the code in full. Here is the description: "`;
-
-
-// function writePrompt(prompt) {
-//     const chatInput = document.getElementById("prompt-textarea");
-//     const sendButton = document.getElementsByClassName("absolute bottom-1.5 right-2 rounded-lg border border-black bg-black p-0.5 text-white transition-colors enabled:bg-black disabled:text-gray-400 disabled:opacity-10 dark:border-white dark:bg-white dark:hover:bg-white md:bottom-3 md:right-3")[0];
-    
-//     chatInput.value = prompt;
-// }
-
-// const footerDiv = document.getElementsByClassName("w-full pt-2 md:pt-0 dark:border-white/20 md:border-transparent md:dark:border-transparent md:w-[calc(100%-.5rem)]")[0];
-// const genWebsiteButton = document.createElement("button")
-// genWebsiteButton.onclick = () => {
-//     let prompt = window.prompt("Website description :")
-//     writePrompt(PROMPT + prompt)
-// }
-// genWebsiteButton.innerText = "Generate Website"
-
-
-// footerDiv.appendChild(genWebsiteButton)
-
-
 setInterval(() => {
     document.querySelectorAll(".language-html").forEach(codeSpace => {
         const existingIframe = codeSpace.querySelector("iframe");
